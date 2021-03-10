acae6e57c4d28e44b2d6e2ecf0f8447024050944
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2021-03-09 20:00:27 -0600

    Added overflow-y in drop area for scrollable it

--- src/components/InputDropArea.js
+++ src/components/InputDropArea.js
@@ -66,7 +66,7 @@ function InputDropArea(props) {
     }
 
     return (
-        <div className="InputDropArea h-64 p-2 bg-indigo-200 hover:bg-indigo-300" onDragEnter={onDragEnter} onDrop={onDrop} onDragOver={onDragOver} onDragLeave={onDragLeave}>
+        <div className="InputDropArea h-64 p-2 bg-indigo-200 hover:bg-indigo-300 overflow-y-auto" onDragEnter={onDragEnter} onDrop={onDrop} onDragOver={onDragOver} onDragLeave={onDragLeave}>
             { files.length === 0 ? <EmptyStateText value={areaMessage} /> : null }
             {
                 files.map((item, index) => (<Item key={index} index={index} value={item} deleteFile={deleteFile} />))
